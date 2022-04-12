62223fdc1b1235b627aad1a7776b91657a38ecb1
Puteri Yudani <puteriyudani@gmail.com>
2022-04-12 20:48:50 +0700

    Project Intent Example 3

---
+++ app/src/main/java/com/puteriyudani/intentexample3/SecondActivity.kt
@@ -0,0 +1,26 @@
+package com.puteriyudani.intentexample3
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_second.*
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        val bundle = intent.getBundleExtra("main_activity_data") //mengekstrak data dari Intent dengan menggunakan method getExtra
+        val height = bundle!!.getFloat("height")
+        val weight = bundle!!.getFloat("weight")
+        txt_intentdata.text = "Height: $height | Weight: $weight"
+        btn_calculate.setOnClickListener {
+            val m_intent = Intent()
+            val m_bmi = 703 * (weight / (height * height))
+            m_intent.putExtra("second_activity_data", m_bmi) //Menambahkan data ke Intent menggunakan method putExtra
+            setResult(Activity.RESULT_OK, m_intent) //Memanggil method setResult
+            finish()
+        }
+    }
+}
